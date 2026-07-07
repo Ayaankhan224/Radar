5b2150015ebbac0efe6f91d8cbeccd75838b82b8
Ayaan Khan <ayaankhan224271@gmail.com>
2026-07-07 09:17:23 +0530

    created login ui

--- frontend/src/features/auth/pages/login.jsx
+++ frontend/src/features/auth/pages/login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const login = () => {
   return (
-    <div>login</div>
-  )
-}
+    <div className="h-screen w-screen flex">
+      <div className="flex-1 bg-amber-300 bg-[url('/login.jpeg')] bg-">
 
-export default login
+      </div>
+      <div className="flex-1 bg-[#fbc604] flex justify-center items-center">
+        <div className="w-[90%] h-[90%] flex flex-col gap-4 justify-center items-center">
+          <h1 className="text-zinc-800 text-9xl uppercase font-mono tracking-tight font-bold">Login</h1>
+
+          <form className="flex flex-col gap-3 w-[80%]">
+            <label htmlFor="email" className="text-zinc-800 text-2xl font-sans tracking-tight font-bold">Enter email</label>
+
+            <input type="text" placeholder="  abc@example.com" className="bg-amber-100 opacity-50 text-gray-700 rounded-full h-12"/>
+
+            <label htmlFor="password" className="text-zinc-800 text-2xl font-sans tracking-tight font-bold">Enter passowrd</label>
+
+            <input type="text" placeholder="&nbsp;&nbsp;******" className="bg-amber-100 opacity-50 text-gray-700 rounded-full h-12"/>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
